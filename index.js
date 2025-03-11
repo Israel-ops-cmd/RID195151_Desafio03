0a92cc897a14b409dba14481ecd2e99ad14ba300
Israel-ops-cmd <israellipe2020@gmail.com>
2025-03-11 15:21:41 -0300

    Ajustando o js e o css

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const inputEtiqueta = document.querySelector('.input-description[name="etiqueta"]');
     const tarefasContainer = document.getElementById('tarefas');
     const finishContainer = document.getElementById('finish');
-    let completedTasksCount = 0;  // Contador de tarefas concluídas
+    let completedTasksCount = 0;  
 
     form.addEventListener('submit', function (event) {
         event.preventDefault();
@@ -30,35 +30,31 @@ document.addEventListener('DOMContentLoaded', function () {
                 </div>
             `;
 
-            tarefasContainer.appendChild(taskElement);
+            tarefasContainer.insertBefore(taskElement, finishContainer);
+
             inputDescription.value = '';
             inputEtiqueta.value = '';
         } else {
             alert('Preencha todos os campos!');
         }
-    });
+    })
 
-    // Evento de clique para concluir tarefas
     tarefasContainer.addEventListener('click', function (event) {
         if (event.target.classList.contains('conclued')) {
             const taskElement = event.target.closest('.tasks');
-            const taskName = taskElement.querySelector('h2'); // Seleciona o nome da tarefa
-
+            const taskName = taskElement.querySelector('h2'); 
             if (taskName) {
-                // Risca o nome da tarefa
+                
                 taskName.style.textDecoration = 'line-through';
                 taskName.style.color = '#B1BACB';
             }
 
-            // Atualiza o contador de tarefas concluídas
             completedTasksCount++;
 
-            // Atualiza o texto no final, no singular ou plural
             const taskWord = completedTasksCount === 1 ? 'Tarefa' : 'Tarefas';
             finishContainer.querySelector('h3').textContent = `${completedTasksCount} ${taskWord} concluída${completedTasksCount > 1 ? 's' : ''}`;
 
-            // Substitui o botão pelo ícone de verificado
             event.target.parentElement.innerHTML = '<img src="assets/verificado.svg" alt="Concluído" width="30" style="margin-right: 3rem;">';
         }
-    });
-});
+    })
+})
